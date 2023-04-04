0e6f6364989ffaa281077009b4a5b291a3b58093
ComputerScienceWarrior <computersciencewarrior@gmail.com>
2023-04-03 23:15:30 -0700

    Creates screen content for Sign Up Page

---
+++ components/screens/sign_up_screen.js
@@ -0,0 +1,15 @@
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+
+export default function SignUpScreen(){
+    return(
+        <View>
+            <View style={{marginBottom: 16}}>
+                <TextInput placeholder='First Name' />
+                <TextInput placeholder='Create A Username' textContentType='username' />
+                <TextInput placeholder='Create A Password' textContentType='password' />
+                <TextInput placeholder='Confirm Password' secureTextEntry={true} />
+            </View>
+            <Button title='Sign Up' />
+        </View>
+    )
+}
