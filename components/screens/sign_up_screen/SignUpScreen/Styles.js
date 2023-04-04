3668bfd55cbe1bb3156d8705eb3a33b06f492e37
ComputerScienceWarrior <computersciencewarrior@gmail.com>
2023-04-03 23:36:03 -0700

    creates file for styling SignUpScreen components

---
+++ components/screens/sign_up_screen/SignUpScreen/Styles.js
@@ -0,0 +1,10 @@
+import { StyleSheet } from 'react-native'; 
+
+export default styles = StyleSheet.create({
+    inputs: {
+        borderWidth: 2,
+        borderColor: 'black',
+        borderRadius: 10,
+        padding: 5,
+    }
+})
