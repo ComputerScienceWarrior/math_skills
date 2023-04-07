44f08711ca884aa5de9b2b452d0647361adcd375
ComputerScienceWarrior <computersciencewarrior@gmail.com>
2023-04-06 20:35:10 -0700

    Creates initial Login Page wrapper

---
+++ components/pages/LoginPage/LoginPage.js
@@ -0,0 +1,12 @@
+import { Text, View } from 'react-native';
+import styles from './Styles';
+
+export default function LoginPage(){
+
+    return(
+        <View>
+            <Text>Login</Text>
+            <LoginScreen />
+        </View>
+    )
+}
