6fef810a5c1cd7f26eb47254db51f7e0111177bb
ComputerScienceWarrior <computersciencewarrior@gmail.com>
2023-04-06 20:46:10 -0700

    adds styling for login screen

---
+++ components/screens/LoginScreen/Styles.js
@@ -0,0 +1,26 @@
+import { StyleSheet } from 'react-native'; 
+
+export default styles = StyleSheet.create({
+    inputs: {
+        borderBottomWidth: 1,
+        borderColor: 'black',
+        borderRadius: 5,
+        padding: 5,
+        margin: 5,
+        width: 200
+    },
+    loginButton: {
+        alignItems: 'center',
+        justifyContent: 'center',
+        paddingVertical: 12,
+        paddingHorizontal: 32,
+        borderRadius: 4,
+        elevation: 3,
+        backgroundColor: 'navy',
+        marginTop: 20
+    },
+    loginText: {
+        fontSize: 20,
+        color: 'white'
+    }
+})
